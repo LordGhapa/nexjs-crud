1e4c92589a93671edd0f5d32cf29c1a00c002569
Lord Ghapa <felipef32@hotmail.com>
2023-09-12 00:17:17 -0300

    🐛 fix: fix import

--- src/templates/Loading/styles.ts
+++ src/templates/Loading/styles.ts
@@ -46,16 +46,16 @@ export const Container = styled.div`
     &:after {
       width: 6rem;
       height: 6rem;
-      border-left: 0.5rem solid ${theme.colors.secondaryColor};
-      border-top: 0.5rem solid ${theme.colors.secondaryColor};
+      border-left: 0.5rem solid ${theme.colors.secondary};
+      border-top: 0.5rem solid ${theme.colors.secondary};
       animation: ${rotate()} 600ms linear infinite;
     }
 
     &:before {
       width: 2rem;
       height: 2rem;
-      border-left: 0.5rem solid ${theme.colors.secondaryColor};
-      border-top: 0.5rem solid ${theme.colors.secondaryColor};
+      border-left: 0.5rem solid ${theme.colors.secondary};
+      border-top: 0.5rem solid ${theme.colors.secondary};
       animation: ${rotate()} 1s linear reverse infinite;
     }
   `}
